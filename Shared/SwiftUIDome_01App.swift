d60637bc18cb678f4b30a0ccfce8465fb780f119
WUYUEZONG <w.yzong@outlook.com>
2021-01-05 15:55:13 +0800

    Initial Commit

---
+++ Shared/SwiftUIDome_01App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDome_01App.swift
+//  Shared
+//
+//  Created by Ginokeo on 2021/1/5.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDome_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
